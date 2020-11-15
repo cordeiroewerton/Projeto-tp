8c99ad9f42dbda13d2da759948571c54c76adfe2
cordeiroewerton <cordeiroewerton13@gmail.com>
2020-11-15 17:29:06 -0300

    att

--- Resource Files/ContaCorrente.cpp
+++ Resource Files/ContaCorrente.cpp
@@ -11,7 +11,7 @@ void ContaCorrente::setTaxaDeManutencao (const int & taxaDeManutencao){
     this-> taxaDeManutencao = taxaDeManutencao;
 }
 void ContaCorrente::CalcularTaxaDeManutencao(const float & taxaDeManutencao){
-    this->taxaDeManutencao=15;
+    this->taxaDeManutencao=taxaDeManutencao;
     /* Como o programa irá funcionar em um tempo menor do que um mês, foi determinado uma única cobrança para
     a taxa de manutenção visto que o programa só irá ficar aberto poucos minutos, e por mais que tivessemos
     criado um relógio, ele seria reiniciado toda vez que abrisse o programa, por tanto não faz sentido cria-lo*/
