324b6b1ff14b7902812d97fd87dd8672c7b6b572
valmyr <65004196+valmyr@users.noreply.github.com>
2020-11-17 22:06:06 -0300

    .

--- Projeto Alternativo sem Bugs/Source Files/mainConta1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
-#include "..\Resource Files\Conta.cpp"
-#include "..\Header Files\ContaPoupanca.h"
-int main(){
-    Conta *b = new ContaPoupanca("Valmir Ferreira da Silva",1289373876,21,300000,{"13737838374","Alagoas","Arapiraca","rt"});
-
-}
