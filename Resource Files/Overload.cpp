431ef588ff6518d798550a555f66f7bf9e218f43
valmyr <65004196+valmyr@users.noreply.github.com>
2020-11-25 16:18:07 -0300

    Update Overload.cpp

--- Resource Files/Overload.cpp
+++ Resource Files/Overload.cpp
@@ -1,5 +1,7 @@
 #include "..\Header Files\Usuario.h"
 #include "..\Header Files\Conta.h"
+#include <stdexcept>
+#include<cmath>
 ostream & operator<<(ostream & os,Usuario & auxUsuario){
 	cout<<"Nome: "<<auxUsuario.getNomeDoUsuario();
 	cout<<"Idade: "<<auxUsuario.getIdade();
@@ -12,19 +14,31 @@ istream & operator>>(istream & is,Usuario & auxUsuario){
 	int idade;
 	long int CPF;
 	Endereco endereco;
-	cout<<"Nome: ";cin>>nome;
-	cout<<"Idade: ";cin>>idade;
-	cout<<"CPF: ";cin>>CPF;
-	cout<<"Pressione Enter"<<endl;
-	cout<<"Rua: ";cin>>endereco.rua;
-	cout<<"Cep: ";cin>>endereco.CEP;
-	cout<<"Cidade: ";cin>>endereco.cidade;
-	cout<<"Estado: ";cin>>endereco.estado;
-	auxUsuario.setNomeDoUsuario(nome);
-	auxUsuario.setIdade(idade);
-	auxUsuario.setCPF(CPF);
-	auxUsuario.setEnderecoDoUsuario(endereco);
-	return is;
+	cin.ignore();
+	try{
+			cout<<"Nome: ";getline(cin,nome);
+			cout<<"Idade: ";cin>>idade;
+			if(cin.fail())
+				throw "idade invalida";
+			cout<<"CPF: ";cin>>CPF;
+			if(cin.fail())
+				throw "CPF invalida";
+			cin.ignore(256,'\n');
+			cout<<"Rua: ";getline(cin,endereco.rua);
+			cout<<"Cep: ";getline(cin,endereco.CEP);
+			cout<<"Cidade: ";getline(cin,endereco.cidade);
+			cout<<"Estado: ";getline(cin,endereco.estado);
+	}catch(const char * e){
+			cin.clear();
+			cin.ignore(256,'\n');
+			cout<<e<<endl;
+			cin>>auxUsuario;
+	}
+		auxUsuario.setNomeDoUsuario(nome);
+		auxUsuario.setIdade(idade);
+		auxUsuario.setCPF(CPF);
+		auxUsuario.setEnderecoDoUsuario(endereco);
+		return is;
 }
 ostream & operator<<(ostream & os,Conta & auxUsuario){
 	cout<<"Nome: "<<auxUsuario.getUser().getNomeDoUsuario()<<endl;
@@ -38,7 +52,17 @@ istream & operator>>(istream & in, Conta & contaAux){
     int senha;
     Usuario auxUser;
     cin>>auxUser;
-    cout<<"senha: ";cin>>senha;
+    cout<<"senha: ";
+		try{
+			cin>>senha;
+			if(cin.fail())
+				throw "Senha constituida apenas de numeros";
+		}catch(const char * e){
+			cout<<e<<endl;
+			while(cin.fail()){
+				cin>>senha;
+			}
+		}
     contaAux.setSenha(senha);
     contaAux.setSaldo(0);
     contaAux.setUser(auxUser);
